34054a85f0e394a1f0e13c4eef1eab8c2c5bc90d
Rassimbm <rassimbenmhamed@outlook.com>
2023-10-11 15:11:47 -0500

    updated index and style-sheet and completed 2nd task: added remove function to script

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log("page loaded ...");
 function showWeatherAlert(element) {
     alert("Loading weather report...");
 }
+
+function acceptCookies(id) {
+    var element = document.querySelector(id);
+    console.log(element);
+    element.remove();
+}
