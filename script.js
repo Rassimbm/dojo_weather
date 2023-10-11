1a6cfca3e38b885af4122487c31a242d24b3e697
Rassimbm <rassimbenmhamed@outlook.com>
2023-10-11 17:52:34 -0500

    updated index and style-sheet and started last task: adding temperature convertion function to script

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 console.log("page loaded ...");
 
-function showWeatherAlert(element) {
+var messageAlrt = document.querySelector(".main-bottom"); 
+
+function showWeatherAlert() {
     alert("Loading weather report...");
 }
 
-function acceptCookies(id) {
-    var element = document.querySelector(id);
-    console.log(element);
-    element.remove();
+function acceptCookies() {
+    messageAlrt.remove();
+}
+
+function celToFah(celsius){
+    return (celsius * 9/5) + 32;
+}
+
+function convertTempUnit(element) {
+    console.log(element.value);
+    for(var i = 1; i < 9; i++){
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = tempSpan.innerText
+        console.log(celToFah(tempVal));
+    }
 }
